13494130b5a413f575646af76566487072a50683
makstravm <maks7-2-94@gmail.com>
2021-01-21 20:10:04 +0200

    added section products week, instal mixitup, fix icon service

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,20 @@
 $(function () {
-  $('.slider-top__slick').slick({
-    arrows: false,
-    dots: true,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000
 
-  });
+  $('.products-week__btn-card, .products-week__btn-wishlist').on('click',  function (){
+    $(this).toggleClass('products-week__btn--active')
+    });
+
+$('.slider-top__slick').slick({
+  arrows: false,
+  dots: true,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
 
 $('.video-promo__link').fancybox({
+});
+
+var mixer = mixitup('.products-week__inner');
 
 })
-
-});
